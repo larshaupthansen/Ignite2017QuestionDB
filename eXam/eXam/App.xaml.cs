ab7d1c0b6e097e611aee8a617d2d9cd9d1f38a70
Lars Haupt-Hansen <Lars.Haupt-Hansen@itera.dk>
2017-09-24 11:31:43 -0400

    Exercise 1-8

--- eXam/eXam/App.xaml.cs
+++ eXam/eXam/App.xaml.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace eXam
 {
     public partial class App : Application
     {
+
+        public static Game CurrentGame { get; set; }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new eXam.MainPage();
+            NavigationPage navigationPage = new NavigationPage(new HomePage());
+            MainPage = navigationPage;
         }
 
         protected override void OnStart()
         {
             // Handle when your app starts
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var resourceStream = assembly.GetManifestResourceStream("eXam.Data.questions.xml");
+            var reader = new StreamReader(resourceStream);
+            var xml = reader.ReadToEnd();
+            var embeddedQuestions = QuizQuestionXmlSerializer.Deserialize(xml);
+            CurrentGame = new Game(embeddedQuestions);
+           
         }
 
         protected override void OnSleep()
---
+++ eXam/eXam/HomePage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace eXam
+{
+    public class HomePage : ContentPage
+    {
+        Button button = null;
+        public HomePage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            AbsoluteLayout layout = new AbsoluteLayout();
+            this.Content = layout;
+
+            Image image = new Image {
+                Source = ImageSource.FromResource("eXam.Images.background.jpg"),
+                Aspect = Aspect.AspectFill };
+
+            layout.Children.Add(
+                view: image, 
+                bounds: new Rectangle(x: 0, y: 0, width: 1, height: 1), 
+                flags: AbsoluteLayoutFlags.SizeProportional);
+
+            this.button = new Button {
+                Text = "Start eXam!",
+                BackgroundColor = Color.LightCoral,
+                Font = Font.SystemFontOfSize(20)
+            };
+            layout.Children.Add(view: button, 
+                bounds: new Rectangle(x: 0.5, y: 0.5, width: 150, height: 60), 
+                flags: AbsoluteLayoutFlags.PositionProportional);
+
+            
+
+        }
+
+        private async void StartButton_Clicked(object sender, EventArgs e)
+        {
+                await this.Navigation.PushAsync(new QuestionPage());         
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.button.Clicked += StartButton_Clicked;
+        }
+
+        protected override void OnDisappearing()
+        {
+            this.button.Clicked -= StartButton_Clicked;
+            base.OnDisappearing();
+        }
+
+    }
+}
